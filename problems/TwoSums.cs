86459cf6765f69d4768a48c355a79dc6c5e41855
Willian Carlos <williancarlos.contato@gmail.com>
2026-09-02 20:08:09 -0300

    TwoSums

--- problems/TwoSums.cs
+++ problems/TwoSums.cs
@@ -1,5 +1,15 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        
+        for(int index = 0; index < nums.Length ; index++){
+            int complement = target - nums[index];
+            
+            for(int compare = index + 1; compare < nums.Length; compare++){
+                if(nums[compare] == complement){
+                    return [index, compare];
+                }
+            }
+        }
+
+        return [0,0];
     }
 }
