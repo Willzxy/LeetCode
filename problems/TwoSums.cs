94a13a5d745eb59f5cc0ed2d76e52528bbb1f957
Willian Carlos <132168791+Willzxy@users.noreply.github.com>
2026-09-09 18:15:40 -0300

    upd

--- problems/TwoSums.cs
+++ problems/TwoSums.cs
@@ -1,15 +1,37 @@
+using System.Numerics;
+
 public class Solution {
-    public int[] TwoSum(int[] nums, int target) {
-        for(int index = 0; index < nums.Length ; index++){
-            int complement = target - nums[index];
-            
-            for(int compare = index + 1; compare < nums.Length; compare++){
-                if(nums[compare] == complement){
-                    return [index, compare];
-                }
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        BigInteger result = 0;
+        BigInteger mult = 1;
+
+        while (l1 != null || l2 != null) {
+            if (l1 != null) {
+                result += l1.val * mult;
+                l1 = l1.next;
             }
+
+            if (l2 != null) {
+                result += l2.val * mult;
+                l2 = l2.next;
+            }
+
+            mult *= 10;
         }
 
-        return [0,0];
+        if (result == 0) {
+            return new ListNode(0);
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode atual = dummy;
+
+        while (result > 0) {
+            atual.next = new ListNode((int)(result % 10));
+            atual = atual.next;
+            result /= 10;
+        }
+
+        return dummy.next;
     }
 }
